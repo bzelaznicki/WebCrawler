281ccd729a66b68459ee11d8126024c9470b4f00
bzelaznicki <bartosz@zelaznicki.com>
2025-05-26 22:50:43 +0200

    starting CLI commands

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+function main() {
+    const args = process.argv.slice(2);
+
+    if (args.length === 0) {
+        console.error("Error: no website provided");
+        process.exit(1);
+    }
+
+    if (args.length > 1) {
+        console.error("Error: too many arguments");
+        process.exit(1);
+    }
+
+    const baseURL = args[0];
+    console.log(`Crawler starting on ${baseURL}...`);
+
+    process.exit(0);
+}
+
+main();
